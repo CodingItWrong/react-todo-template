b72e93c121c2a4cfacc812cda52f11ae06086a1c
Josh Justice <me@codingitwrong.com>
2021-05-09 18:17:59 -0400

    Add Prettier and custom ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'jest', 'cypress'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': 'warn',
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
+};
